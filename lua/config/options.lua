be15ac183acbc799639a73bbefc7f2de0d528b15
Hongzheng Li <hongzheng@cs.wisc.edu>
2025-02-23 11:22:35 -0600

    feat: clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,24 @@ vim.opt.tabstop = 4 -- Number of spaces a <Tab> counts for
 vim.opt.shiftwidth = 4 -- Number of spaces used for auto-indentation
 vim.opt.expandtab = true -- Use spaces instead of tabs
 -- vim.g.autoformat = false
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
