2d775d28c782cea2d5cda775255e5a81ca7b13bc
Hongzheng Li <hongzheng@cs.wisc.edu>
2025-05-25 12:11:36 -0500

    Yank whole file && Markdown render

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>yy", function()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  vim.cmd("keepjumps normal! ggyG")
+  vim.api.nvim_win_set_cursor(0, pos)
+end, { desc = "Yank Entire File" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
 vim.opt.tabstop = 4 -- Number of spaces a <Tab> counts for
+-- vim.opt.tabstop = 2 -- Number of spaces a <Tab> counts for
+
 vim.opt.shiftwidth = 4 -- Number of spaces used for auto-indentation
+-- vim.opt.shiftwidth = 2 -- Number of spaces used for auto-indentation
+
 vim.opt.expandtab = true -- Use spaces instead of tabs
+-- vim.opt.expandtab = false
+
 -- vim.g.autoformat = false
 
 vim.o.clipboard = "unnamedplus"
