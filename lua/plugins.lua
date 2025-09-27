5947e4130f6983d50c1ed290e8b4959a8103be45
GoldenBadger <roymanclee@gmail.com>
2025-09-26 20:31:33 -0500

    update config

---
+++ lua/plugins.lua
@@ -0,0 +1,50 @@
+return {
+    "p00f/clangd_extensions.nvim",
+    ft = { "c", "cpp" },
+    config = function()
+        require("clangd_extensions").setup({
+            server = {
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy",
+                    "--header-insertion=iwyu",
+                    "--completion-style=detailed",
+                    "--function-arg-placeholders",
+                    "--fallback-style=llvm",
+                },
+                init_options = {
+                    usePlaceholders = true,
+                    completeUnimported = true,
+                    clangdFileStatus = true,
+                },
+            },
+            extensions = {
+                autoSetHints = true,
+                inlay_hints = {
+                    inline = false,
+                    only_current_line = false,
+                    only_current_line_autocmd = "CursorHold",
+                    show_parameter_hints = true,
+                    parameter_hints_prefix = "<- ",
+                    other_hints_prefix = "=> ",
+                    max_len_align = false,
+                    max_len_align_padding = 1,
+                    right_align = false,
+                    right_align_padding = 7,
+                    highlight = "Comment",
+                },
+            },
+        })
+
+        local lspconfig = require("lspconfig")
+        lspconfig.clangd.setup({
+            on_attach = function(client, bufnr)
+                require("clangd_extensions.inlay_hints").setup_autocmd()
+                require("clangd_extensions.inlay_hints").set_inlay_hints()
+                vim.keymap.set("n", "<leader>lh", ":ClangdToggleInlayHints<CR>", { buffer = bufnr })
+                -- more clangd_extensions keymaps if desired
+            end,
+        })
+    end,
+}
--- lua/plugins/vscode-gates.lua
+++ lua/plugins/vscode-gates.lua
@@ -1,6 +1,5 @@
 -- Disable VS-Code-hostile extras
 return {
-    { "echasnovski/mini.animate",      cond = not vim.g.vscode },
-    { "sphamba/smear-cursor.nvim",     cond = not vim.g.vscode },
-  }
-  
+    { "nvim-mini/mini.animate", cond = not vim.g.vscode },
+    { "sphamba/smear-cursor.nvim", cond = not vim.g.vscode },
+}
